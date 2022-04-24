f0d0d2dc0bb947297899b6272cf7ff83287c0d47
JosipMuzic-KTO <josip.m@kto.com>
2022-04-24 16:27:27 +0200

    Add AppNavigator to SplashScreen

--- lib/src/screens/splash.dart
+++ lib/src/screens/splash.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:rombmarketingstrategy/src/screens/form.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:rombmarketingstrategy/src/utils/app_navigator.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -13,17 +14,14 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  static const _redirectDelay = Duration(seconds: 4);
+
   @override
   void initState() {
     super.initState();
     Timer(
-      const Duration(seconds: 3),
-      () => Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => const FormScreen(),
-        ),
-      ),
+      _redirectDelay,
+      () => AppNavigator.to(context, () => const FormScreen()),
     );
   }
 
