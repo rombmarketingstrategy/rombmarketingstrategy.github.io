a916cb45d9e4778fc131bf0a732a6d431c74d377
JosipMuzic-KTO <josip.m@kto.com>
2022-04-24 16:33:37 +0200

    Add Spacers and Paddings utils classes

---
+++ lib/src/utils/paddings.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Paddings {
+  static const a4 = EdgeInsets.all(4);
+  static const a8 = EdgeInsets.all(8);
+  static const a16 = EdgeInsets.all(16);
+  static const a32 = EdgeInsets.all(32);
+
+  static const h4 = EdgeInsets.symmetric(horizontal: 4);
+  static const h8 = EdgeInsets.symmetric(horizontal: 8);
+  static const h16 = EdgeInsets.symmetric(horizontal: 16);
+  static const h32 = EdgeInsets.symmetric(horizontal: 32);
+
+  static const v4 = EdgeInsets.symmetric(vertical: 4);
+  static const v8 = EdgeInsets.symmetric(vertical: 8);
+  static const v16 = EdgeInsets.symmetric(vertical: 16);
+  static const v32 = EdgeInsets.symmetric(vertical: 32);
+}
---
+++ lib/src/utils/spacers.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Spacers {
+  static const h8 = SizedBox(height: 8);
+  static const h16 = SizedBox(height: 16);
+  static const h32 = SizedBox(height: 32);
+  static const h64 = SizedBox(height: 64);
+
+  static const w4 = SizedBox(width: 4);
+  static const w8 = SizedBox(width: 8);
+  static const w16 = SizedBox(width: 16);
+  static const w32 = SizedBox(width: 32);
+  static const w64 = SizedBox(width: 64);
+
+  static const expanded = Expanded(child: SizedBox());
+}
