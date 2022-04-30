d4ec8450fda1f8ccd2de8c56155e261600cf0a92
JosipMuzic-KTO <josip.m@kto.com>
2022-04-30 12:25:16 +0200

    Add check if has internet in FormService

--- lib/src/services/form_service.dart
+++ lib/src/services/form_service.dart
@@ -1,21 +1,30 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:rombmarketingstrategy/src/models/form_data.dart';
 import 'package:rombmarketingstrategy/src/services/local_storage_service.dart';
 
 class FormService {
   FormService._();
-  static late CollectionReference collection;
+  static CollectionReference? collection;
   static List<FormData> formDatas = [];
 
   static Future<void> init() async {
+    if (await hasInternet()) {
     collection = FirebaseFirestore.instance.collection('form');
+  }
   }
 
   static Future<void> addFormData(FormData formData) async {
-    await collection.add(formData.toMap());
+    if (!(await hasInternet())) return LocalStorageService.addNewData(formData);
+
+    if (collection == null) init();
+    await collection?.add(formData.toMap());
   }
 
   static Future<void> addSavedFormData() async {
+    if (collection == null) init();
     final allData = LocalStorageService.dataToSend;
     final List<Future> futures = [];
     for (final formData in allData) {
@@ -25,5 +34,17 @@ class FormService {
     }
     await Future.wait(futures);
     LocalStorageService.clear();
+  }
+
+  static Future<bool> hasInternet() async {
+    if (kIsWeb) return true;
+
+    try {
+      final result = await InternetAddress.lookup('google.com');
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) return true;
+    } on SocketException catch (_) {
+      return false;
+    }
+    return false;
   }
 }
